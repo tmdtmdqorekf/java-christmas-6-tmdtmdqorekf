26a4d5d3b870ea02f393564f8198f42f15ed9265
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-14 12:42:20 +0900

    docs: TODO main에 작성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,6 +2,26 @@ package christmas;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 우테코 식당 소개 출력
+
+        // TODO: 예상 방문 날짜 입력 받기
+
+        // TODO: 주문 메뉴, 메뉴 개수 입력 받기
+
+        // TODO: 우테코 식당 인트로 출력
+
+        // TODO: 주문 메뉴 출력
+
+        // TODO: 할인 전 총주문 금액 계산 및 출력
+
+        // TODO: 증정 메뉴 계산 및 출력
+
+        // TODO: 혜택 내역 계산 및 출력
+
+        // TODO: 총혜택 금액 계산 및 출력
+
+        // TODO: 할인 후 예상 결제 금액 계산 및 출력
+
+        // TODO: 12월 이벤트 배지 계산 및 출력
     }
 }
