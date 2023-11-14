eeafa405d5ebe0d11390672f2f16aebdd0e4ef54
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-14 13:08:06 +0900

    feat: 주문 메뉴, 메뉴 개수 입력 및 출력

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,8 +1,28 @@
 package christmas;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
+    static class Menu {
+        private String food;
+        private int quantity;
+
+        public Menu(String food, int quantity) {
+            this.food = food;
+            this.quantity = quantity;
+        }
+
+        public String getFood() {
+            return food;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+    }
     public static void main(String[] args) {
         // TODO: 우테코 식당 소개 출력
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
@@ -13,6 +33,17 @@ public class Application {
         int visitDate = Integer.parseInt(visitDateStr);
 
         // TODO: 주문 메뉴, 메뉴 개수 입력 받기
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1");
+        String menuStr = Console.readLine();
+
+        List<String> menus = Arrays.asList(menuStr.split(","));
+        List<Menu> menuList = menus.stream()
+                .map(menu -> menu.split("-"))
+                .map(m -> new Menu(m[0], Integer.parseInt(m[1])))
+                .collect(Collectors.toList());
+
+        System.out.println("<주문 메뉴>");
+        menuList.forEach(menu -> System.out.println(menu.getFood() + " " + menu.getQuantity() + "개"));
 
         // TODO: 우테코 식당 인트로 출력
 
