0692c86f6aa2f724b5232e0b318f98c945498c6c
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-15 17:06:22 +0900

    refactor: 할인 전 총주문 금액 출력 및 계산

--- src/main/java/christmas/model/Customer.java
+++ src/main/java/christmas/model/Customer.java
@@ -1,10 +1,27 @@
 package christmas.model;
 
+import static christmas.model.Menu.getMenu;
 import static christmas.view.InputView.askVisitDate;
+
+import java.util.List;
 
 public class Customer {
     public static int getVisitDate() {
         final int VISITDATE = askVisitDate();
         return VISITDATE;
+    }
+
+    public static int getUserOrderPrice(List<Order> orderList) {
+        int userOrderPrice = 0;
+
+        for (Order order : orderList) {
+            Menu userOrder = Menu.getMenu(order.food());
+            userOrderPrice += getEachPrice(order, userOrder);
+        }
+        return userOrderPrice;
+    }
+
+    private static int getEachPrice(Order order, Menu userOrder) {
+        return userOrder.price * order.quantity();
     }
 }
