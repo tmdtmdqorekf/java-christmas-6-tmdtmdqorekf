d9a542102da8ad1b67b9ec8fa48bce912b16914e
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-15 15:17:08 +0900

    refactor: enum 파일 생성 및 옮기기

---
+++ src/test/java/christmas/model/Menu.java
@@ -0,0 +1,33 @@
+package christmas.model;
+
+public enum Menu {
+    양송이수프("애피타이저", 6_000),
+    타파스("애피타이저", 5_500),
+    시저샐러드("애피타이저", 8_000),
+    티본스테이크("메인", 55_000),
+    바비큐립("메인", 54_000),
+    해산물파스타("메인", 35_000),
+    크리스마스파스타("메인", 25_000),
+    초코케이크("디저트", 15_000),
+    아이스크림("디저트", 5_000),
+    제로콜라("음료", 3_000),
+    레드와인("음료", 60_000),
+    샴페인("음료", 25_000);
+
+    private final String type;
+    private final int price;
+
+    Menu(String type, int price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    private static Menu getMenu(String menuName) {
+        for (Menu menu : Menu.values()) {
+            if (menu.name().equals(menuName)) {
+                return menu;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/christmas/model/Week.java
@@ -0,0 +1,30 @@
+package christmas.model;
+
+public enum Week {
+    일("SUNDAY", "평일", true),
+    월("MONDAY", "평일", false),
+    화("TUESDAY", "평일", false),
+    수("WEDNESDAY", "평일", false),
+    목("THURSDAY", "평일", false),
+    금("FRIDAY", "주말", false),
+    토("SATURDAY", "주말", false);
+
+    private final String weekName;
+    private final String weekdayOrWeekend;
+    private final boolean isStarDay;
+
+    Week(String weekName, String weekdayOrWeekend, boolean isStarDay) {
+        this.weekName = weekName;
+        this.weekdayOrWeekend = weekdayOrWeekend;
+        this.isStarDay = isStarDay;
+    }
+
+    private static Week getWeekdayOrWeekend(String dayOfWeek) {
+        for (Week day : Week.values()) {
+            if (day.weekName.equals(dayOfWeek)) {
+                return day;
+            }
+        }
+        return null;
+    }
+}
